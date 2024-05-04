312f55244fef37ed89760a51bdcf291a4e92a343
AhmadKayyali <ahmadkayy@gmail.com>
2024-05-04 14:27:37 +1000

    App file changes

--- locked-in/src/App.js
+++ locked-in/src/App.js
@@ -1,7 +1,16 @@
+import React, { useEffect } from 'react';
 import logo from './logo.png';
 import './App.css';
 
-function App() {
+function App({ onAnimationEnd }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onAnimationEnd();
+    }, 5000); // The duration matches the CSS animation time.
+
+    return () => clearTimeout(timer);
+  }, [onAnimationEnd]);
+
   return (
     <div className="App">
       <img src={logo} className="logo" alt="logo" />
--- locked-in/src/index.js
+++ locked-in/src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import HomePage from './HomePage';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+function Main() {
+  const [animationDone, setAnimationDone] = useState(false);
+
+  const handleAnimationEnd = () => {
+    setAnimationDone(true); // This function will be called when the animation in App finishes
+  };
+
+  return (
+    <React.StrictMode>
+      {!animationDone ? <App onAnimationEnd={handleAnimationEnd} /> : <HomePage />}
+    </React.StrictMode>
+  );
+}
+
+root.render(<Main />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
