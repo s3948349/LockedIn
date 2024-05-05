de507e1e3992c570efeb66456d7b324bc8db36e7
Suriya Sivakumar <s3948349@student.rmit.edu.au>
2024-05-05 13:29:11 +1000

    added flask backend files

---
+++ locked-in/backend/flask_conn.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/execute_script')
+def execute_script():
+    # TODO change the script path and args
+    script_path = ''
+    arg1 = 'value1'
+    arg2 = 'value2'
+
+    # Execute the Python script with arguments
+    try:
+        result = subprocess.run(['python', script_path, arg1, arg2], capture_output=True, text=True, check=True)
+        stdout = result.stdout
+        stderr = result.stderr
+        return jsonify({'stdout': stdout, 'stderr': stderr})
+    except subprocess.CalledProcessError as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
